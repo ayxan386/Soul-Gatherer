b2b12ae0ea391adea67774a0cbbd9ab8e593de07
ayxan386 <ayxanpro@gmail.com>
2023-02-22 13:18:08 +0100

    Item interaction basics

---
+++ Assets/Scripts/Player/ItemInteraction.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class ItemInteraction : MonoBehaviour
+{
+    [Header("Raycast params")] [SerializeField]
+    private Transform origin;
+
+    [SerializeField] private Transform direction;
+    [SerializeField] private float reachDistance;
+    [SerializeField] private LayerMask interactionLayer;
+
+    [Header("Others")] [SerializeField] private GameObject interactableDisplay;
+
+    void Update()
+    {
+        if (Physics.Raycast(origin.position, direction.forward, out RaycastHit hit, reachDistance, interactionLayer))
+        {
+            if (hit.collider.TryGetComponent(out InteractableItem item))
+            {
+                if (Input.GetButtonDown("Interact"))
+                {
+                    item.Interact();
+                }
+
+                interactableDisplay.SetActive(true);
+            }
+        }
+        else
+        {
+            interactableDisplay.SetActive(false);
+        }
+    }
+}
