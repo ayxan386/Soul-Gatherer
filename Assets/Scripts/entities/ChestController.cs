4050b3494f0477395f384776975f88c4ce2654e3
ayxan386 <ayxanpro@gmail.com>
2023-02-22 12:51:23 +0100

    Added prefabs for chest and script

---
+++ Assets/Scripts/entities/ChestController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using UnityEngine;
+
+public class ChestController : ItemInteractionBehavior
+{
+    [SerializeField] private Transform lidTransform;
+    [SerializeField] private float speed;
+    [SerializeField] private float maxAngle;
+
+    public override void Interact(InteractionPassData data)
+    {
+        StartCoroutine(OpenLid());
+    }
+
+    private IEnumerator OpenLid()
+    {
+        float currentAngle = lidTransform.eulerAngles.x;
+        while (Mathf.Abs(currentAngle) < maxAngle)
+        {
+            currentAngle += speed * Time.deltaTime;
+            lidTransform.Rotate(speed * Time.deltaTime, 0, 0);
+            yield return new WaitForEndOfFrame();
+        }
+
+        Complete = true;
+    }
+}
---
+++ Assets/Scripts/entities/InteractableItem.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableItem : MonoBehaviour
+{
+    [SerializeField] protected List<ItemInteractionBehavior> interactionBehaviors;
+    protected bool wasInteracted;
+
+    [ContextMenu("Interact")]
+    public void Interact()
+    {
+        print("Interacting");
+        StartCoroutine(Interaction(new InteractionPassData(wasInteracted)));
+    }
+
+    private IEnumerator Interaction(InteractionPassData data)
+    {
+        foreach (var behavior in interactionBehaviors)
+        {
+            yield return new WaitForSeconds(behavior.DelayBefore);
+            behavior.Interact(data);
+            yield return new WaitUntil(() => behavior.Complete);
+            yield return new WaitForSeconds(behavior.DelayAfter);
+        }
+    }
+}
+
+public abstract class ItemInteractionBehavior : MonoBehaviour
+{
+    public float DelayBefore;
+    public float DelayAfter;
+    public bool Complete;
+
+    public abstract void Interact(InteractionPassData data);
+}
+
+public class InteractionPassData
+{
+    public bool WasInteractedBefore;
+
+    public InteractionPassData(bool wasInteractedBefore)
+    {
+        this.WasInteractedBefore = wasInteractedBefore;
+    }
+
+    public InteractionPassData()
+    {
+        WasInteractedBefore = false;
+    }
+}
