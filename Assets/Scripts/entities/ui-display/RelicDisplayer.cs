01e5319874e12cc7988393ff44fb3f604fc78937
ayxan386 <ayxanpro@gmail.com>
2023-03-18 17:16:38 +0100

    Basic ui elements

---
+++ Assets/Scripts/entities/ui-display/RelicDisplayer.cs
@@ -0,0 +1,18 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RelicDisplayer : MonoBehaviour
+{
+    [SerializeField] private Image icon;
+    [SerializeField] private TextMeshProUGUI description;
+    [SerializeField] private GameObject descriptionHolder;
+
+    public string relicName;
+    
+    private Relic
+
+    void UpdateUi()
+    {
+    }
+}
