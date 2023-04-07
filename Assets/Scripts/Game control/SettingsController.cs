060c38750e918bf8d5ed3ca6ee84239d2936aef6
ayxan386 <ayxanpro@gmail.com>
2023-04-07 12:50:52 +0200

    Added slider for gamepad sensitivity

--- Assets/Scripts/Game control/SettingsController.cs
+++ Assets/Scripts/Game control/SettingsController.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
@@ -5,16 +6,40 @@ using UnityEngine.UI;
 
 public class SettingsController : MonoBehaviour
 {
-    [SerializeField] private Toggle shadowToggle;
-
+    [SerializeField] private GameObject settingsMenuUi;
     private const string ProcessingShadows = "processing-shadows";
+    private const string GamepadsensitivityMult = "GamepadSensitivityMult";
+
+    private Toggle shadowToggle;
     private Volume globalVolume;
+    private Slider gamepadSensitivitySlider;
+    private TextMeshProUGUI gamepadSenValueText;
 
     private void Start()
     {
-        shadowToggle.onValueChanged.AddListener(OnProcessingStateChange);
         GameObject.FindWithTag("GlobalVolume")?.TryGetComponent(out globalVolume);
+        gamepadSensitivitySlider = settingsMenuUi.transform.Find("GamepadSensitivity").GetComponent<Slider>();
+        gamepadSenValueText = gamepadSensitivitySlider.transform.Find("value").GetComponent<TextMeshProUGUI>();
+        shadowToggle = settingsMenuUi.transform.Find("PostProcessingEffects").GetComponent<Toggle>();
+
+        shadowToggle.onValueChanged.AddListener(OnProcessingStateChange);
+        gamepadSensitivitySlider.onValueChanged.AddListener(OnGamepadSensitivityChanged);
+
+        settingsMenuUi.SetActive(false);
+
         UpdateShadowProcessing(PlayerPrefs.GetInt(ProcessingShadows) == 1);
+        OnGamepadSensitivityChanged(PlayerPrefs.GetFloat(GamepadsensitivityMult));
+    }
+
+    private void OnGamepadSensitivityChanged(float val)
+    {
+        PlayerPrefs.SetFloat(GamepadsensitivityMult, val);
+        gamepadSensitivitySlider.value = val;
+        gamepadSenValueText.text = $"x {val:N1}";
+        if (Camera.main.TryGetComponent(out CameraMovement cm))
+        {
+            cm.GamepadSensitivityMult = val;
+        }
     }
 
     public void OnProcessingStateChange(bool state)
